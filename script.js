bfac5595e836ad2303c1d4a4b4c23c6a6bd01a5f
Taha <taha-chaoui@hotmail.com>
2022-01-19 21:04:30 +0100

    30%

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ function operate(a, b, operator) {
 console.log(operate(1, 5, "+"));
 
 // Next
+let container = document.getElementById("container");
+let digits = document.querySelectorAll(".digits");
+let results = document.getElementById("results");
+let clear = document.getElementById("clear");
+
+results.textContent = "0";
+
+digits.forEach((digit) => {
+  digit.addEventListener("click", (e) => {
+    document.getElementById("results").textContent = `${e.target.textContent}`;
+  });
+});
+
+clear.addEventListener("click", () => {
+  document.getElementById("results").textContent = "0";
+});
